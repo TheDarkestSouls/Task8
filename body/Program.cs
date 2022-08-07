4edd7e955d3b0dcc469410f4a840859645e499ed
NikitaP <unntiss@bk.ru>
2022-08-07 19:26:01 +0300

    done

--- body/Program.cs
+++ body/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+if (N < 2) Console.WriteLine("Something wrong...");
+else
+{
+    while (count < N)
+    {
+        Console.WriteLine(count);
+        count += 2;
+    }
+    N %= 2;
+    if(N == 0) 
+    Console.WriteLine(N);
+}
